1d69eba39903382cdba062ad555726b08e3f1fae
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-15 00:02:38 +0100

    Order: tests update

--- Server/Order/services/__tests__/orderServices.test.ts
+++ Server/Order/services/__tests__/orderServices.test.ts
@@ -1,72 +1,44 @@
 import request from 'supertest';
 import express from 'express';
-import orderRouter from '../orderServices'
+import bodyParser from 'body-parser';
+import orderRouter from '../orderServices'; // Adjust the import path as necessary
+import * as OrderController from '../../controllers/ordersController';
+import  checkAuth  from '../../middlewares/check-auth';
 
+// Mock middlewares and controller methods
+jest.mock('../../middlewares/check-auth', () => ({
+    verifyToken: jest.fn((req, res, next) => next()),
+}));
+
+jest.mock('../../controllers/ordersController', () => ({
+  addOrder: jest.fn((req, res) => res.status(201).send({ message: "Order added" })),
+  findCompletedOrders: jest.fn((req, res) => res.status(200).send([])),
+}));
+
+// Create express application and apply the order router
 const app = express();
+app.use(bodyParser.json());
+app.use('/orders', orderRouter);
 
-
-
-// Sample authentication middleware for testing purposes
-const checkAuth = {
-  verifyToken: (req: any, res: any, next: any) => {
-    // Mock authentication logic (you can replace this with your actual logic)
-    const token = req.headers.authorization;
-
-    if (!token || token !== 'Bearer validToken') {
-      return res.status(401).json({ error: 'Authentication failed' });
-    }
-
-    next();
-  },
-};
-
-// Sample OrderController for testing purposes
-const OrderController = {
-  addOrder: (req: any, res: any) => {
-    // Mock adding an order
-    res.status(200).json({ message: 'Order added successfully' });
-  },
-  findCompletedOrders: (req: any, res: any) => {
-    // Mock fetching completed orders
-    res.status(200).json({ orders: ['Order1', 'Order2'] });
-  },
-};
-
-// Use the router and middleware in your Express app
-app.use(express.json());
-app.use('/api/orders', orderRouter);
-
-describe('Order API Endpoints', () => {
-  // Test for the POST route
-  describe('POST /api/orders/addOrder', () => {
-    it('should add a new order', async () => {
-      const response = await request(app)
-        .post('/api/orders/addOrder')
-        .set('Authorization', 'Bearer validToken') // Replace with a valid token if needed
-        .send({ /* your request body here */ });
-
-      expect(response.statusCode).toBe(200);
-      expect(response.body).toHaveProperty('message', 'Order added successfully');
-    });
-
-    it('should return an error if authentication fails', async () => {
-      const response = await request(app)
-        .post('/api/orders/addOrder')
-        .send({ /* your request body here */ });
-
-      expect(response.statusCode).toBe(401);
-      expect(response.body).toHaveProperty('error', 'Authentication failed');
-    });
+describe('Order Services Routes', () => {
+  it('should add an order', async () => {
+    const orderData = { item: 'Sample Item', quantity: 1 }; // replace with your actual order data
+    await request(app)
+      .post('/orders/addOrder')
+      .send(orderData)
+      .expect(201);
+    expect(OrderController.addOrder).toHaveBeenCalled();
   });
 
-  // Test for the GET route
-  describe('GET /api/orders/findCompletedOrders', () => {
-    it('should fetch all completed orders', async () => {
-      const response = await request(app)
-        .get('/api/orders/findCompletedOrders');
+  it('should fetch all completed orders', async () => {
+    await request(app)
+      .get('/orders/findCompletedOrders')
+      .expect(200);
+    expect(OrderController.findCompletedOrders).toHaveBeenCalled();
+  });
 
-      expect(response.statusCode).toBe(200);
-      // Add more assertions as needed for the response data
-    });
+  // Reset the mocks after each test
+  afterEach(() => {
+    jest.clearAllMocks();
   });
 });
