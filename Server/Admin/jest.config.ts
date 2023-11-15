0241641d50cca7660a1f44ca8889378149cb372b
Andrii Voznesenskyi <01174367@pw.edu.pl>
2023-11-15 01:04:49 +0100

    Update jest config

--- Server/Admin/jest.config.ts
+++ Server/Admin/jest.config.ts
@@ -1,5 +1,13 @@
-module.exports = {
-    preset: 'ts-jest',
-    testEnvironment: 'node',
-  };
-  
+import type { Config } from '@jest/types';
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  globals: {
+    'ts-jest': {
+      tsconfig: 'tsconfig.json'
+    }
+  }
+};
+
+export default config;
